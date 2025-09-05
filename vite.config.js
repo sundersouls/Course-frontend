9d64734d09c789a297005e334c6e5600c3864649
sundersouls <k22570@list.ru>
2025-09-05 17:47:30 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/course/',
+  server: {
+    host: true,
+    allowedHosts: ['fleetbase.paylab.kz'], 
+  },
 })
